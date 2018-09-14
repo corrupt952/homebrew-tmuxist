bad4e0a5d167892588940b6039d8a05bb165d874
Kazuki Hasegawa <hasegawa@khasegawa.net>
2018-09-14 12:09:28 +0900

    bumpup 0.0.2

--- tmuxist.rb
+++ tmuxist.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Tmuxist < Formula
   homepage 'https://github.com/corrupt952/tmuxist'
-  url "https://github.com/corrupt952/tmuxist/releases/download/0.0.1/tmuxist_0.0.1_darwin_amd64.tar.gz"
-  sha256 "4cc0e92554c2024734f92de2e35742903266727bfd88c9febc23d191e30520d6"
+  url "https://github.com/corrupt952/tmuxist/releases/download/0.0.2/tmuxist_0.0.2_darwin_amd64.tar.gz"
+  sha256 "97bfe5a9a0f2e5fd50452596614383bd8cf5b3b2e9747501a8d3983e91e7b94e"
   head 'https://github.com/corrupt952/tmuxist.git'
-  version '0.0.1'
+  version '0.0.2'
 
   def install
     bin.install "tmuxist"
