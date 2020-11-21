c816ba927e266bf045f0507e26e8018d07310636
Kazuki HASEGAWA <k@zuki.dev>
2020-11-21 18:56:25 +0900

    bumpup 0.0.6

--- tmuxist.rb
+++ tmuxist.rb
@@ -6,11 +6,11 @@ class Tmuxist < Formula
   version '0.0.5'
 
   if `uname -s` == 'Darwin'
-    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.5/tmuxist_0.0.5_darwin_amd64.tar.gz"
-    sha256 "3ea413d7163fae6fe5c2ef2cc8a8ccd7fbb211061d3ac7490e12d7fc9063f362"
+    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.6/tmuxist_0.0.6_darwin_amd64.tar.gz"
+    sha256 "a4dabcae72c7da19365b1963bb0e3d3d8c2ed73aa7c64c7123c59aa570d03b7d"
   else
-    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.5/tmuxist_0.0.5_linux_amd64.tar.gz"
-    sha256 "7640e758da33b54bdc139960d5d4b48217b4164b231aaba2dae6785d6ff952c4"
+    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.6/tmuxist_0.0.6_linux_amd64.tar.gz"
+    sha256 "dc4cd6905f0e6931cf1e1dc1078a6468f8561cb0f700df96a9be541f61d80b5e"
   end
 
   def install
