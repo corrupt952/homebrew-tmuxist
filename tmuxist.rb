7cce827652b8905225fd7d0bcc1c274aba3ffeab
Kazuki HASEGAWA <k@zuki.dev>
2021-01-03 06:18:17 +0900

    bumpup 0.0.7

--- tmuxist.rb
+++ tmuxist.rb
@@ -3,14 +3,14 @@ require "formula"
 class Tmuxist < Formula
   homepage 'https://github.com/corrupt952/tmuxist'
   head 'https://github.com/corrupt952/tmuxist.git'
-  version '0.0.6'
+  version '0.0.7'
 
   if `uname -s` == 'Darwin'
-    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.6/tmuxist_0.0.6_darwin_amd64.tar.gz"
-    sha256 "a4dabcae72c7da19365b1963bb0e3d3d8c2ed73aa7c64c7123c59aa570d03b7d"
+    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.7/tmuxist_0.0.7_darwin_amd64.tar.gz"
+    sha256 "6a8891c0982113127b84cf46ea3755eaebaa2b40763c8f86352636f26a5a6984"
   else
-    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.6/tmuxist_0.0.6_linux_amd64.tar.gz"
-    sha256 "dc4cd6905f0e6931cf1e1dc1078a6468f8561cb0f700df96a9be541f61d80b5e"
+    url "https://github.com/corrupt952/tmuxist/releases/download/0.0.7/tmuxist_0.0.7_linux_amd64.tar.gz"
+    sha256 "bdba7ec05f15f963de200bdd97b71ebb7620c7171cc283f6be1f7236b46717ff"
   end
 
   def install
